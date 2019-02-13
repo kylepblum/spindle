97325220f0ba05da7aefee542a1d680ab26f8572
Kyle Phillip Blum <20462558+kylepblum@users.noreply.github.com>
2019-02-13 17:53:19 -0600

    Continued developing code for trial_data inputs

--- getAffPotFromMusState.m
+++ getAffPotFromMusState.m
@@ -1,18 +1,15 @@
 function trial_data = getAffPotFromMusState(trial_data,params)
 
-if ~isfield(params,'gammaMode')
-    params.gammaMode = 'const';
-end
 
-%%%%%%%%%%%%%%%%% Deal with gamma signals %%%%%%%%%%%%%%%%%%%%%
-if strcmp(params.gammaMode,'const')
-    gammaS = 0.5;
-    gammaD = 0.5;
-elseif strcmp(params.gammaMode,'agca')
-    % these will be determined point-by-point within the loop
-    gammaS = [];
-    gammaD = [];
-end
+
+% TODOs
+% EMG normalization?
+% Muscle length normalization?
+% 
+
+emgInd = find(strcmp(params.emgName,td_s(1).emg_names));
+musInd = find(strcmp(params.musName,td_s(1).muscle_names));
+
 
 
 %%%%%%%%%%%%%%%%
@@ -22,13 +19,17 @@ numSims = numel(trial_data);       % Number of simulations to run in parallel
 
 parfor a = 1:numSims
 % Take out relevant data from trial a and pad so model can initialize
-t = 0:trial_data(a).bin_size:trial_data.idx_startTime
+t = (0:trial_data(a).bin_size:trial_data(a).idx_endTime)';
+delta_gammaD = trial_data(a).;
+delta_gammaS = [];
+delta_cdl = [];
+
     
     
     
 % Use MATLAB's built-in parallel computing to run simulations and store
 % data: 
-    [hsB(a),dataB(a),hsC(a),dataC(a)] = sarcSimDriver(t,delta_f_activated(a,:),delta_cdl(a,:));
+    [hsB(a),dataB(a),hsC(a),dataC(a)] = sarcSimDriver(t,delta_gammaD,delta_gammaS,delta_cdl(a,:));
     [r(a),~,~] = sarc2spindle(dataB(a),dataC(a),1,1,0.03,1,0.09);
     disp(['Done with simulation number ' num2str(a)])
 end
