25573e778fc5e9b834e4651ce75bb8ff893d8e68
Kyle Phillip Blum <20462558+kylepblum@users.noreply.github.com>
2019-04-22 14:14:53 -0500

    Made example look more like classical data

--- example.m
+++ example.m
@@ -14,7 +14,7 @@ delta_f_activated = zeros(numSims,numel(t));
 
 % Specify base value of parameter that may change in each loop. In this 
 % case, we want to vary the duration of the ramp phase of stretch:
-pertDur = 0.3/time_step;
+pertDur = 0.6/time_step;
 
 % length scaling factor to account for pinnation & elastic attachment of 
 % fibers:
@@ -27,11 +27,11 @@ for a = 1:numSims
         if i == 1
             delta_f_activated(a,i) = 0.3;
         elseif i > pertStart && i < pertStart + pertDur/a
-            delta_f_activated(a,i) = 0.01*a;
-            delta_cdl(a,i) = -118.2*a*lsf*time_step;
-%             delta_cdl(a,i) = 118.2*a*lsf*time_step*0.01;
+            %delta_f_activated(a,i) = 0.01*a;
+            %delta_cdl(a,i) = -118.2*a*lsf*time_step;
+            delta_cdl(a,i) = 118.2*a*lsf*time_step;
         elseif i > pertStart + pertDur/a + 1/time_step && i < pertStart + 2*(pertDur/a) + 1/time_step
-%             delta_cdl(a,i) = -118.2*a*lsf*time_step;
+            delta_cdl(a,i) = -118.2*a*lsf*time_step;
         end
     end
 end
@@ -46,7 +46,7 @@ end
 toc;
 hold on; plot(dataB(4).t,dataB(4).hs_force,dataC(4).t,dataC(4).hs_force)
 
-[r,rd,rs] = sarc2spindle(dataB(4),dataC(4),1.5,2,0.03,1,0);
+[r,rd,rs] = sarc2spindle(dataB(4),dataC(4),1.5,1.0,0.015,1,0);
 figure
 hold on;
 plot(dataB(4).t,r); plot(dataB(4).t,rd); plot(dataB(4).t,rs)
